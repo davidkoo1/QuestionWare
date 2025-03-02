6158fdcf7c0e4eadf3590db6ad0203acffe6e3d4
davidkoo1 <sverceks@gmail.com>
2025-03-02 04:27:29 +0200

    Set quiz infra layer

--- src/Microservices/Quiz/Api/QuestionWare.Quiz.Api/Program.cs
+++ src/Microservices/Quiz/Api/QuestionWare.Quiz.Api/Program.cs
@@ -1,8 +1,12 @@
+using QuestionWare.Quiz.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/Microservices/Quiz/Infrastructure/QuestionWare.Quiz.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuestionWare.Quiz.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IDbConnection>(sp =>
+            new SqlConnection(configuration.GetConnectionString("LocalConnection")));
+
+        return services;
+    }
+}
+
